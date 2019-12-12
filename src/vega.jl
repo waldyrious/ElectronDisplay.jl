813e753a725c8192c17ee4b1cb007b776f315441
David Anthoff <anthoff@berkeley.edu>
2019-12-12 01:57:37 +0000

    Format files using DocumentFormat

--- src/vega.jl
+++ src/vega.jl
@@ -7,11 +7,9 @@ Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vega.v5+
 
 function _maybe_fallback_to(mime, d, x, payload)
     if sizeof(payload) > d.config.max_json_bytes
-        @warn string(
-            "The size of JSON representation (", sizeof(payload), ")",
+        @warn string("The size of JSON representation (", sizeof(payload), ")",
             " exceeds `max_json_bytes = ", d.config.max_json_bytes, "`.",
-            " Falling back to $mime."
-        )
+            " Falling back to $mime.")
         return display(d, mime, x)
     end
     return nothing
@@ -21,14 +19,14 @@ function _display_vegalite(d, major_version_vegalite, major_version_vega, x)
     payload = stringmime(MIME("application/vnd.vegalite.v$major_version_vegalite+json"), x)
     ans = _maybe_fallback_to(MIME"image/png"(), d, x, payload)
     ans === nothing || return ans
-    displayplot(d, "vega-lite", JSON.JSONText(payload), options=Dict("webPreferences" => Dict("webSecurity" => false)))
+    displayplot(d, "vega-lite", JSON.JSONText(payload), options = Dict("webPreferences" => Dict("webSecurity" => false)))
 end
 
 function _display_vega(d, major_version, x)
     payload = stringmime(MIME("application/vnd.vega.v$major_version+json"), x)
     ans = _maybe_fallback_to(MIME"image/png"(), d, x, payload)
     ans === nothing || return ans
-    displayplot(d, "vega", JSON.JSONText(payload), options=Dict("webPreferences" => Dict("webSecurity" => false)))  
+    displayplot(d, "vega", JSON.JSONText(payload), options = Dict("webPreferences" => Dict("webSecurity" => false)))
 end
 
 function Base.display(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vegalite.v2+json")}, x)
